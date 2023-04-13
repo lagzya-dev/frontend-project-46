e07d3364844c37ec6711225b8251d5c9db1eeb86
lagzya-dev <you@example.com>
2023-04-13 11:54:46 +0300

    refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,37 +1,21 @@
 import { test, expect } from '@jest/globals';
-import path from 'path';
 import genDiff from '../src/index.js';
 import { stylish, plain, json } from '../__fixtures__/result.js';
+import { getPath } from '../src/utils.js';
 
-const getPath = (way) => (way.includes('__fixtures__') ? path.resolve(process.cwd(), way) : path.resolve(process.cwd(), `__fixtures__/${way}`));
-
-test('Test JSON', () => {
+test('Tests', () => {
   const file1 = getPath('file1.json');
   const file2 = getPath('file2.json');
+
+  const file3 = getPath('file1.yaml');
+  const file4 = getPath('file2.yaml');
+
+  const file5 = getPath('file1.yml');
+  const file6 = getPath('file2.yml');
   const stylishTry = genDiff(file1, file2, 'stylish').trim();
   expect(stylishTry).toEqual(stylish);
-  const plainTry = genDiff(file1, file2, 'plain').trim();
+  const plainTry = genDiff(file3, file4, 'plain').trim();
   expect(plainTry).toEqual(plain);
-  const jsonTry = genDiff(file1, file2, 'json').trim();
-  expect(jsonTry).toEqual(json);
-});
-test('Test YAML', () => {
-  const file1 = getPath('file1.yaml');
-  const file2 = getPath('file2.yaml');
-  const stylishTry = genDiff(file1, file2, 'stylish').trim();
-  expect(stylishTry).toEqual(stylish);
-  const plainTry = genDiff(file1, file2, 'plain').trim();
-  expect(plainTry).toEqual(plain);
-  const jsonTry = genDiff(file1, file2, 'json').trim();
-  expect(jsonTry).toEqual(json);
-});
-test('Test YML', () => {
-  const file1 = getPath('file1.yml');
-  const file2 = getPath('file2.yml');
-  const stylishTry = genDiff(file1, file2, 'stylish').trim();
-  expect(stylishTry).toEqual(stylish);
-  const plainTry = genDiff(file1, file2, 'plain').trim();
-  expect(plainTry).toEqual(plain);
-  const jsonTry = genDiff(file1, file2, 'json').trim();
+  const jsonTry = genDiff(file5, file6, 'json').trim();
   expect(jsonTry).toEqual(json);
 });
