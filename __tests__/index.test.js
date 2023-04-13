33782e3ef122f969d7d661b47091fc707fdb06b9
lagzya-dev <you@example.com>
2023-04-13 12:01:34 +0300

    test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,19 +3,19 @@ import genDiff from '../src/index.js';
 import { stylish, plain, json } from '../__fixtures__/result.js';
 import { getPath } from '../src/utils.js';
 
-test('Tests', () => {
-  const file1 = getPath('file1.json');
-  const file2 = getPath('file2.json');
-
-  const file3 = getPath('file1.yaml');
-  const file4 = getPath('file2.yaml');
-
-  const file5 = getPath('file1.yml');
-  const file6 = getPath('file2.yml');
-  const stylishTry = genDiff(file1, file2, 'stylish').trim();
-  expect(stylishTry).toEqual(stylish);
-  const plainTry = genDiff(file3, file4, 'plain').trim();
-  expect(plainTry).toEqual(plain);
-  const jsonTry = genDiff(file5, file6, 'json').trim();
-  expect(jsonTry).toEqual(json);
+describe('comparing  files', () => {
+  test('Tests', () => {
+    const file1 = getPath('file1.json');
+    const file2 = getPath('file2.json');
+    const file3 = getPath('file1.yaml');
+    const file4 = getPath('file2.yaml');
+    const file5 = getPath('file1.yml');
+    const file6 = getPath('file2.yml');
+    const stylishTry = genDiff(file1, file2, 'stylish').trim();
+    expect(stylishTry).toEqual(stylish);
+    const plainTry = genDiff(file3, file4, 'plain').trim();
+    expect(plainTry).toEqual(plain);
+    const jsonTry = genDiff(file5, file6, 'json').trim();
+    expect(jsonTry).toEqual(json);
+  });
 });
